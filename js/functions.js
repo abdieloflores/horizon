afdf151a571e51c5bd723aa7d8be08732f735bd6
Otoniel Flores <abdiel_flores@outlook.com>
2020-10-07 18:45:51 -0500

    Update 07-10-20

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,18 @@
 let nav = document.getElementById("nav");
 let logo = document.getElementById("logo");
 let nav2 = document.getElementById("nav2");
+let toggle1 = document.getElementById("toggle1");
 
 window.addEventListener("scroll", e=>{
     if(window.scrollY > 100){
         nav.className = "navbar fixed-top navbar-dark bg-dark navbar-expand-lg navbar-template"
         logo.className = "navbar-brand"
         nav2.className = "navbar-nav ml-auto"
-
+        toggle1.className = "d-flex flex-row order-2 order-lg-3 ml-auto"
     }else{
         nav.className = "navbar fixed-top navbar-light bg-white navbar-expand-lg navbar-template"
         logo.className = "navbar-brand d-none"
         nav2.className = "navbar-nav m-auto"
+        toggle1.className = "d-flex flex-row order-2 order-lg-3 m-auto"
     }
 });
